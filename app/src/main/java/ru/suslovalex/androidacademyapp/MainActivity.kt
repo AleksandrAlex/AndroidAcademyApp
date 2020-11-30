f984d882bf0f0233bcad37fd54530debb9c77651
AleksandrAlex <suslovalmi@mail.ru>
2020-11-30 11:47:07 +0300

    homework 3: fragments and ui component

--- app/src/main/java/ru/suslovalex/androidacademyapp/MainActivity.kt
+++ app/src/main/java/ru/suslovalex/androidacademyapp/MainActivity.kt
@@ -3,9 +3,27 @@ package ru.suslovalex.androidacademyapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentMoviesList.ClickItemListener {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        setContentView(R.layout.activity_main)
+
+        initFragmentMoviesList()
+
+    }
+
+    private fun initFragmentMoviesList() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container_layout, FragmentMoviesList())
+            .commit()
+    }
+
+    override fun moveToMovieDetails() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container_layout, FragmentMoviesDetails())
+            .addToBackStack(null)
+            .commit()
     }
 }
